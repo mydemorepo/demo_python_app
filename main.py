e23c591a426fbc1f6246674d5f04457571417ec4
mydemorepo <adamvasyliuta@gmail.com>
2023-02-04 20:30:28 +0200

    Added service for mysql operation.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import *
+from services.mysqldb import mysqldb
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    mysql_db = mysqldb.MysqlDb("localhost", "root", "root", "classicmodels",)
+    data = mysql_db.get_result("SHOW TABLES")
+    return render_template('main.html', data = data)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
