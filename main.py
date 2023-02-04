9fb737c132bde14662e2bd4ae7e1fbb17d9e9769
mydemorepo <adamvasyliuta@gmail.com>
2023-02-04 17:04:56 +0200

    Added template system.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
