550ce7a9f51a464a4ec031e0ebf609b6d9b16b42
Adam Vasyliuta <adamvasyliuta@gmail.com>
2023-02-06 10:57:24 +0200

    Display of tables on the web page has been implemented.

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def index():
 def index2(table_name):
     mysql_db = mysqldb.MysqlDb("localhost", "root", "root", "classicmodels",)
     tables_list = mysql_db.get_result("SHOW TABLES")
-    return render_template('main.html', tables_list=tables_list, table_name = table_name)
+    table = mysql_db.get_result(f"SELECT * FROM {table_name}")
+    return render_template('main.html', tables_list=tables_list, table = table)
 
 
 if __name__ == '__main__':
