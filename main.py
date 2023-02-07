df5efd4539ce18d5702eb0aae0b2c89a12608cb4
Adam Vasyliuta <adamvasyliuta@gmail.com>
2023-02-07 20:52:08 +0200

    Implemented returning a selection from Sqlite in "dict list" format.

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from services.mysqldb import mysqldb
 from services.sqllitedb import sqllitedb
 app = Flask(__name__)
 
+#  Sqlite is used by default.
+#  To use MySql, install MySql 
+#  Server and restore the 
+#  sampledatabase.sql database 
+#  from the dump.
 
-mysql_db = mysqldb.MysqlDb("localhost", "root", "root", "classicmodels",)
-sqllite_db = sqllitedb.SqlliteDb("data.sqlite")
+#db = mysqldb.MysqlDb("localhost", "root", "root", "classicmodels",)
+
+db = sqllitedb.SqlliteDb("sampledatabase.sqlite")
 
 
 @app.route('/')
 def index():
-    tables_list = mysql_db.get_tables()
-    tables_listlite = sqllite_db.get_result(f"SELECT * FROM offices")
-    return render_template('main.html', tables_list=tables_list, tables_listlite = tables_listlite)
+    tables_list = db.get_tables()
+    return render_template('main.html', tables_list=tables_list)
 
 @app.route('/<table_name>')
 def tables(table_name):
-    tables_list = mysql_db.get_tables()
-    table = mysql_db.get_result(f"SELECT * FROM {table_name}")
+    tables_list = db.get_tables()
+    table = db.get_result(f"SELECT * FROM {table_name}")
     return render_template('tables.html', tables_list=tables_list, table = table)
 
 
--- services/sqllitedb/sqllitedb.py
+++ services/sqllitedb/sqllitedb.py
@@ -9,17 +9,35 @@ class SqlliteDb():
     def get_result(self, query_string):
         db = sqlite3.connect(self.db_name)
         cursor = db.cursor()
+        
         cursor.execute(query_string)
         data = cursor.fetchall()
+        
+        cursor.execute(f'PRAGMA table_info({query_string.split()[-1]})')
+        column_info = cursor.fetchall()
         db.close()
-        return data
+        
+        table_content = []
+        for line in data:
+            line_dict = {}
+            for cell_content, colum_name in zip(line, column_info):
+                line_dict[colum_name[1]] = cell_content
+            table_content.append(line_dict)
+               
+        db.close()
+        
+        return table_content
     
     def get_tables(self):
         db = sqlite3.connect(self.db_name)
         cursor = db.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type ='table'")
-        data = cursor.fetchall()
+        table_names = cursor.fetchall()
         db.close()
-        return data
-    
+        
+        tables_list = []
+        for table in table_names:          
+            tables_list.append({'Table_name':table[0]})
+       
+        return tables_list
     
