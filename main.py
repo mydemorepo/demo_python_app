144aa10dae0cf8775d9509ce3c57f4930747c9fe
Adam Vasyliuta <adamvasyliuta@gmail.com>
2023-02-07 10:55:13 +0200

    Corrected incorrect display of web pages.

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def index():
     return render_template('main.html', tables_list=tables_list)
 
 @app.route('/<table_name>')
-def index2(table_name):
+def tables(table_name):
     mysql_db = mysqldb.MysqlDb("localhost", "root", "root", "classicmodels",)
     tables_list = mysql_db.get_result("SHOW TABLES")
     table = mysql_db.get_result(f"SELECT * FROM {table_name}")
-    return render_template('main.html', tables_list=tables_list, table = table)
+    return render_template('tables.html', tables_list=tables_list, table = table)
 
 
 if __name__ == '__main__':
